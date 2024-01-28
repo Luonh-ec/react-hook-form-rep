fe0969718b11aadf4a7d409356c3d8ed01b10fe0
LeHaGiaBao <lehagiabao0205@gmail.com>
2024-01-28 13:28:21 +0700

    Update form login

--- src/components/InputBox/InputBox.tsx
+++ src/components/InputBox/InputBox.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import { KeyboardTypeOptions, StyleSheet, TextInput } from 'react-native';
+import { KeyboardTypeOptions, StyleSheet, Text, TextInput, View } from 'react-native';
 
 interface InputBoxCustomProps {
     nativeID: string,
     value: string,
     onChangeText: (text: string) => void,
-    onBlur: (text: any) => void,
-    keyboardType?: KeyboardTypeOptions,
-    secureTextEntry?: boolean,
+    // onBlur: (text: any) => void,
     placeholder: any,
 }
 
@@ -15,26 +13,27 @@ const InputBox: React.FC<InputBoxCustomProps> = ({
     nativeID,
     value,
     onChangeText,
-    onBlur,
-    keyboardType,
-    secureTextEntry,
+    // onBlur,
     placeholder
 }) => {
     return (
-        <TextInput
-            nativeID={nativeID}
-            value={value}
-            onChangeText={onChangeText}
-            onBlur={onBlur}
-            keyboardType={keyboardType}
-            secureTextEntry={secureTextEntry}
-            placeholder={placeholder}
-            style={styles.input}
-        />
+        <View style={styles.container}>
+            <TextInput
+                nativeID={nativeID}
+                value={value}
+                onChangeText={onChangeText}
+                // onBlur={onBlur}
+                placeholder={placeholder}
+                style={styles.input}
+            />
+        </View>
     );
 }
 
 const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+    },
     input: {
         height: 40,
         borderColor: 'gray',
--- src/screen/Login/Login.tsx
+++ src/screen/Login/Login.tsx
@@ -1,12 +1,54 @@
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import LogoImage from '../../components/LogoImage';
+import InputBox from '../../components/InputBox';
 
 const Login = () => {
+    const [formData, setFormData] = useState({
+        username: '',
+        email: '',
+        password: '',
+        confirmpassword: '',
+    })
+
+
+    const onChangeInput = (e: any) => {
+        const formField = e.target.name
+        const updateFormData = {
+            ...formData,
+            [formField]: e.target.value
+        }
+        setFormData(updateFormData)
+    }
+
     return (
         <>
             <View style={styles.container}>
                 <LogoImage />
+                <InputBox
+                    nativeID='username'
+                    value={formData.username}
+                    placeholder="username"
+                    onChangeText={onChangeInput}
+                />
+                <InputBox
+                    nativeID='email'
+                    value={formData.email}
+                    placeholder="email"
+                    onChangeText={onChangeInput}
+                />
+                <InputBox
+                    nativeID='password'
+                    value={formData.password}
+                    placeholder="password"
+                    onChangeText={onChangeInput}
+                />
+                <InputBox
+                    nativeID='confirmpassword'
+                    value={formData.confirmpassword}
+                    placeholder="confirmpassword"
+                    onChangeText={onChangeInput}
+                />
             </View>
         </>
     );
