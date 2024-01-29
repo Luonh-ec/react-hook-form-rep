0743fdea33d107519f3f9bf46b50465588bd0fb2
LeHaGiaBao <lehagiabao0205@gmail.com>
2024-01-29 17:29:57 +0700

    Update project

--- src/screens/UseFormLogin.tsx
+++ src/screens/UseFormLogin.tsx
@@ -1,20 +1,14 @@
 import React from 'react';
 import {
-  Button,
   StyleSheet,
   Text,
   TextInput,
   TouchableOpacity,
   View,
 } from 'react-native';
-import {useForm} from '../hooks/useForm';
+import { useForm } from '../hooks/useForm';
 import LogoImage from '../components/LogoImage';
-import {validateEmail, validatePassword} from '../helpers/validationChecks';
-
-interface Login {
-  email: string;
-  password: string;
-}
+import { validateEmail, validatePassword } from '../helpers/validationChecks';
 
 const UseFormLogin = () => {
   const initialValues = {
@@ -27,11 +21,11 @@ const UseFormLogin = () => {
     password: validatePassword,
   };
 
-  const onSubmit = (values: {[key: string]: string}) => {
+  const onSubmit = (values: { [key: string]: string }) => {
     console.log('Form submitted with values: ', values);
   };
 
-  const {values, errors, handleChange, handleSubmit} = useForm({
+  const { values, errors, handleChange, handleSubmit } = useForm({
     initialValues,
     onSubmit,
     validationRules,
@@ -47,7 +41,7 @@ const UseFormLogin = () => {
         onChangeText={text => handleChange('email', text)}
         style={styles.input}
       />
-      {errors.email && <Text style={{color: 'red'}}>{errors.email}</Text>}
+      {errors.email && <Text style={{ color: 'red' }}>{errors.email}</Text>}
       <TextInput
         autoCapitalize="none"
         secureTextEntry
@@ -56,7 +50,7 @@ const UseFormLogin = () => {
         onChangeText={text => handleChange('password', text)}
         style={styles.input}
       />
-      {errors.password && <Text style={{color: 'red'}}>{errors.password}</Text>}
+      {errors.password && <Text style={{ color: 'red' }}>{errors.password}</Text>}
       <TouchableOpacity style={styles.loginButton} onPress={handleSubmit}>
         <Text style={styles.buttonText}>Login</Text>
       </TouchableOpacity>
